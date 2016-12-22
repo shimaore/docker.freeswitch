3538a56fad9d76a56bf569bd9c7e14fd0bc3ad8c
Stephane Alnet <stephane@shimaore.net>
2016-12-22 01:56:37 +0100

    latest

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo 'Starting build.'
 cd /home/freeswitch
 git clone -b test-FS-9776 https://freeswitch.org/stash/scm/~stephalnet/freeswitch.git freeswitch.git
 cd freeswitch.git
-git checkout d0acd9b091ecdcf3b8c1d91cf5bf68c0c754a9e1
+git checkout 174951a94dc0f3506c0d1b4fb283839a365893d9
 cp /tmp/modules.conf.in build/modules.conf.in
 sh bootstrap.sh -j
 ./configure --prefix=/opt/freeswitch
