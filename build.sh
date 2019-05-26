747029ed8db5a16cf5b66f274dc90a3049f52b07
Stephane Alnet <stephane@shimaore.net>
2019-05-26 21:54:10 +0200

    back to master

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo 'Starting build.'
 cd $HOME
 git clone -b master https://gitlab.k-net.fr/ccnq/freeswitch-original.git freeswitch.git
 cd freeswitch.git
-git checkout 9127c41316b41f1bb00f72515de3f825595f6e09
+git checkout 886b2d39aa418e8e4ca67335c4448f79f1da9cc5
 cp /tmp/modules.conf.in build/modules.conf.in
 for f in /tmp/patches/*; do patch -p1 < $f; done
 sh bootstrap.sh -j
